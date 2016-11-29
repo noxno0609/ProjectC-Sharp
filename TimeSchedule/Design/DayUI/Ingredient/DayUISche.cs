bd78ecbe86a26707208f0aaafd28a9db38477903
BenX <onspeedxxzero@gmail.com>
2016-11-29 22:41:01 +0700

    [Main] Update New Project Application Name.

--- TimeSchedule/Design/DayUI/Ingredient/DayUISche.cs
+++ TimeSchedule/Design/DayUI/Ingredient/DayUISche.cs
@@ -38,5 +38,10 @@ namespace WindowsFormsApplication1
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
         }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
--- TimeSchedule/MainMenu.Designer.cs
+++ TimeSchedule/MainMenu.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace TimeScheduleMain
 {
     partial class MainMenu
     {
