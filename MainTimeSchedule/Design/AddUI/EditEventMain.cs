3e8be49924722b864cfb9a6b3eae1d978f37e10c
theho120696 <dinh2993@gmail.com>
2016-12-10 00:07:52 +0700

    update add UI

---
+++ MainTimeSchedule/Design/AddUI/EditEventMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TSProject.Design.EditEvent.Ingredient;
+
+namespace MainTimeSchedule.Design.AddUI
+{
+    public partial class EditEventMain : Form
+    {
+        public EditEventMain()
+        {
+            InitializeComponent();
+        }
+
+        public void showControl(System.Windows.Forms.Control obj)
+        {
+            panelcontrol.Controls.Clear();
+            obj.Dock = DockStyle.Fill;
+            panelcontrol.Controls.Add(obj);
+        }
+
+
+        private void radioButtonday_CheckedChanged(object sender, EventArgs e)
+        {
+            DayEventUI dayform = new DayEventUI();
+            showControl(dayform);
+        }
+
+        private void radioButtonweek_CheckedChanged(object sender, EventArgs e)
+        {
+            WeekEventUI weekform = new WeekEventUI();
+            showControl(weekform);
+        }
+    }
+}
