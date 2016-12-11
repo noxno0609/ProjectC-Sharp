7042fd2cb01e5264747b22df7b3029d700d6f0d0
pobilove2 <pobilove2@gmail.com>
2016-12-11 15:02:32 +0700

    up code periodeventDTO.

---
+++ SQLTimeSchedule/DTO/PeriodEventDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLTS
+{
+    public class PeriodEventDTO
+    {
+        public int PE_ID;
+        public string Note;
+        public DateTime DateStart;
+        public DateTime DateEnd;
+        public string DaySelect;
+        public DateTime TimeStart;
+        public DateTime TimeEnd;
+       
+    }
+}
