3be2c7a79ae7527230704bed5b16370f767f62be
BenX <onspeedxxzero@gmail.com>
2016-12-12 22:05:11 +0700

    Fix New Update.

--- MainTimeSchedule/Design/MainUI/WeekUI/Ingredient/WeekUITool.cs
+++ MainTimeSchedule/Design/MainUI/WeekUI/Ingredient/WeekUITool.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TSProject.Design.EditEvent.Ingredient;
 
 namespace TSProject.Design.WeekUI.Ingredient
 {
