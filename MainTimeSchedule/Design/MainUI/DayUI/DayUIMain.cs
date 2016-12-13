898d796fb9645356e5bfdffae05b96c05be3a50d
vanminhphan2 <vanminhpha2@gmai.com>
2016-12-13 23:13:23 +0700

    up code

--- MainTimeSchedule/Design/MainUI/DayUI/DayUIMain.cs
+++ MainTimeSchedule/Design/MainUI/DayUI/DayUIMain.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApplication1.DayUI.Ingredient;
+using WindowsFormsApplication1.Design.WeekUI;
 
 namespace WindowsFormsApplication1.DayUI
 {
@@ -16,6 +17,14 @@ namespace WindowsFormsApplication1.DayUI
         public DayUIMain()
         {
             InitializeComponent();
+            dayUITool.buttonchange.Click += new EventHandler(ButtonSwitch_Click);
+        }
+        private void ButtonSwitch_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            WeekUIMain dayform = new WeekUIMain();
+            dayform.Dock = DockStyle.Fill;
+            this.Controls.Add(dayform);
         }
     }
 }
