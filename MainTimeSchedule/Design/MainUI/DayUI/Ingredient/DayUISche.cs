092718f9f1e4185f5d48e6af48d12049ee654d35
vanminhphan2 <vanminhpha2@gmai.com>
2016-12-16 22:27:44 +0700

    update code khung 24h

--- MainTimeSchedule/Design/MainUI/DayUI/Ingredient/DayUISche.cs
+++ MainTimeSchedule/Design/MainUI/DayUI/Ingredient/DayUISche.cs
@@ -22,20 +22,19 @@ namespace WindowsFormsApplication1
         public static void setNumberforTable(TableLayoutPanel table)
         {
             for(int i=0; i<table.ColumnCount; i++)
-            {
+            { 
                 string number = Convert.ToString(i+1);
-                TextBox textbox = new TextBox();
+                Label textbox = new Label();
                 textbox.Text = number;
-                textbox.TextAlign = HorizontalAlignment.Center;
+                textbox.TextAlign = ContentAlignment.MiddleCenter;
                 // textbox.FlatStyle = FlatStyle.System;
                 textbox.AutoSize = false;
                 textbox.BorderStyle = BorderStyle.FixedSingle;
                 textbox.Padding = new Padding(0);
                 textbox.Margin = new Padding(0);
                 textbox.Dock = DockStyle.Fill;
-                textbox.Padding = new Padding(0, textbox.Height / 2, 0, 0);
-                textbox.BackColor = Color.LightGray;
-                //textbox.TextAlign = ContentAlignment.MiddleCenter;
+                textbox.Padding = new Padding(0, textbox.Height/6, 0, 0);
+                //textbox.BackColor = Color.LightGray;
                 table.Controls.Add(textbox, i, 0);
 
             }
