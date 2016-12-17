2b4c82cb515e0d2e7afc6e1cbf2f889b42a7975a
pobilove2 <pobilove2@gmail.com>
2016-12-17 11:54:30 +0700

    upc#

--- SQLTimeSchedule/DAO/PeriodEventDAO/GetAllDTO.cs
+++ SQLTimeSchedule/DAO/PeriodEventDAO/GetAllDTO.cs
@@ -25,8 +25,8 @@ namespace SQLTS
             {
                 PeriodEventDTO dto = new PeriodEventDTO();
                 dto.ID = Convert.ToInt32(dr["PE_ID"]);
-                dto.DateStart = Convert.ToDateTime(dr["DayStart"]);
-                dto.DateEnd = Convert.ToDateTime(dr["DayEnd"]);
+                dto.DateStart = Convert.ToDateTime(dr["DateStart"]);
+                dto.DateEnd = Convert.ToDateTime(dr["DateEnd"]);
                 dto.TimeStart = SQLFormat.formatTime(dr["TimeStart"].ToString());
                 dto.TimeEnd = SQLFormat.formatTime(dr["TimeEnd"].ToString());
                 dto.DaySelect = Convert.ToString(dr["Dayselect"]);
