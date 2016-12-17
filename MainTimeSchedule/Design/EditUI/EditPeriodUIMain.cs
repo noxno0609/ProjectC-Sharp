62d4fb813d0f8965cf54ec06dc5e81c804f58142
vanminhphan2 <vanminhpha2@gmai.com>
2016-12-17 12:25:07 +0700

    update code

--- MainTimeSchedule/Design/EditUI/EditPeriodUIMain.cs
+++ MainTimeSchedule/Design/EditUI/EditPeriodUIMain.cs
@@ -16,12 +16,6 @@ namespace TSProject.Design.EditEvent
         {
             InitializeComponent();
         }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void button3_Click(object sender, EventArgs e)
         { 
             //DialogResult answer;
