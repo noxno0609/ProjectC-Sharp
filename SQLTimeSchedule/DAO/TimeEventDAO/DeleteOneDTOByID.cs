e0fed5a2a01448d9bec890401ed3efb1855682ca
tranhuynhtuan <tranhuynhtuan20@gmail.com>
2016-12-18 18:39:20 +0700

    up code

---
+++ SQLTimeSchedule/DAO/TimeEventDAO/DeleteOneDTOByID.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLCommon;
+
+namespace SQLTS
+{
+    public partial class TimeEventDAO
+    {
+        public static void DeleteOneDTOByID(TimeEventDTO dto, MySqlConnection conn)
+        {
+            conn.Open();
+            string sql = string.Format(@"DELETE FROM timeevent WHERE TE_ID = '{0}'", dto.ID);
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
