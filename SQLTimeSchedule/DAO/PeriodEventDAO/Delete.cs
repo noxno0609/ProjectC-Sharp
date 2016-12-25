8cc4298b2bea9ea278e9d101310bed8a00b85d64
BenX <onspeedxxzero@gmail.com>
2016-12-25 12:02:14 +0700

    Update missing PeriodDAO.

---
+++ SQLTimeSchedule/DAO/PeriodEventDAO/Delete.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLCommon;
+using System.Data;
+using SQLTS;
+
+namespace SQLTS
+{
+    public partial class PeriodEventDAO
+    {
+        public static bool Delete(PeriodEventDTO dto, MySqlConnection conn)
+        {
+            conn.Open();
+
+            string sql = "DELETE FROM periodevent WHERE PE_ID = " + dto.ID;
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            int periodresult = cmd.ExecuteNonQuery();
+
+            sql = "DELETE FROM timeevent WHERE PE_ID = " + dto.ID;
+            cmd = new MySqlCommand(sql, conn);
+            int timeresult = cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            return (periodresult > 1 && timeresult > 1) ? true : false;
+        }
+    }
+}
---
+++ SQLTimeSchedule/DAO/PeriodEventDAO/Insert.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLCommon;
+using System.Data;
+using SQLTS;
+
+namespace SQLTS
+{
+    public partial class PeriodEventDAO
+    {
+        public static int Insert(PeriodEventDTO dto, MySqlConnection conn)
+        {
+            conn.Open();
+
+            #region Insert Period Event
+            string sql = String.Format(@"INSERT INTO PeriodEvent (Note, TimeStart, TimeEnd, DaySelect, DateStart, DateEnd)
+                                        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')",
+
+            dto.Note,
+            SQLFormat.addDateTime(dto.TimeStart),
+            SQLFormat.addDateTime(dto.TimeEnd),
+            dto.DaySelect,
+            SQLFormat.addDateTime(dto.TimeStart),
+            SQLFormat.addDateTime(dto.DateEnd));
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            int newId = Convert.ToInt32(cmd.LastInsertedId);
+            #endregion
+
+
+            #region Insert Time Event Depend On Period
+            //
+            List<DayOfWeek> list = SQLFormat.formatDaySelect(dto.DaySelect);
+            for (DateTime date = dto.DateStart; date <= dto.DateEnd; date = date.AddDays(1))
+            {
+                if (list.Contains(date.DayOfWeek) == true)
+                {
+                    TimeEventDTO dtotemp = new TimeEventDTO();
+                    dtotemp.PE_ID = newId;
+                    dtotemp.Note = dto.Note;
+                    dtotemp.TimeStart = dto.TimeStart;
+                    dtotemp.TimeEnd = dto.TimeEnd;
+                    dtotemp.DaySelect = date;
+                    TimeEventDAO.Insert(dtotemp, conn);
+                }
+            }
+            #endregion
+
+            conn.Close();
+            return newId;
+        }
+    }
+}
